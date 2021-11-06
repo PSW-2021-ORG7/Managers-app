6d05e05c83fc19dfbb2bb5e64006474867e05bda
RA57-2018 <bugarski.ra57.2018@uns.ac.rs>
2021-11-06 16:45:49 +0100

    feat: Add frontend for feedback

---
+++ Frontend/src/app/integration/pharmacy-registration/pharmacy-registration.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PharmacyService {
+    private baseUrl: string = 'http://localhost:44298/api/pharmacies';
+
+    constructor(private http: HttpClient) { }
+
+    createNewPharmacy(obj: any): Observable<any> {
+        return this.http.post(this.baseUrl, obj);
+    }
+}
