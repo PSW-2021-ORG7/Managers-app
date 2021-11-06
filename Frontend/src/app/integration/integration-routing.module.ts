a4229ceeeca986d4ebdfe993185c684d601bf383
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2021-11-06 10:25:01 +0100

    feat: Add feedback for pharmacies

--- Frontend/src/app/integration/integration-routing.module.ts
+++ Frontend/src/app/integration/integration-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateFeedbackComponent } from './create-feedback/create-feedback.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { IntegrationComponent } from './integration.component';
 import { PharmacyRegistrationComponent } from './pharmacy-registration/pharmacy-registration.component';
 
 const routes: Routes = [
   { path: '', component: IntegrationComponent },
-  { path : 'pharmacy-registration', component: PharmacyRegistrationComponent }
+  { path : 'pharmacy-registration', component: PharmacyRegistrationComponent },
+  { path : 'feedback', component: FeedbackComponent },
+  { path : 'create-feedback', component: CreateFeedbackComponent }
    
 ];
 
