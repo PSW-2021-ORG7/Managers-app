c52bdd047d18c1ecc83a939f0c3b27652e3fba1d
vujsa7 <61715001+vujsa7@users.noreply.github.com>
2021-11-07 19:00:06 +0100

    feat: Create UI element to display room info and form to update room info

--- Frontend/src/app/hospital-map/hospital-map.module.ts
+++ Frontend/src/app/hospital-map/hospital-map.module.ts
@@ -6,11 +6,13 @@ import { HospitalMapComponent } from './hospital-map.component';
 import { D3Service } from './shared/d3.service';
 import { FloorPlanComponent } from './floor-plan/floor-plan.component';
 import { FormsModule } from '@angular/forms';
+import { RoomInfoFormComponent } from './floor-plan/room-info-form/room-info-form.component';
 
 @NgModule({
   declarations: [
     HospitalMapComponent,
-    FloorPlanComponent
+    FloorPlanComponent,
+    RoomInfoFormComponent
   ],
   imports: [
     CommonModule,
