1b4c278312f28b767f6a5992c4b174099dd21ed5
DejanBjelic <dejan.bjelic@uns.ac.rs>
2021-11-07 23:23:39 +0100

    feat: Added option to see response for sent feedback

--- Frontend/src/app/integration/create-feedback/create-feedback.component.ts
+++ Frontend/src/app/integration/create-feedback/create-feedback.component.ts
@@ -1,29 +1,34 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CreateFeedbackService } from './create-feedback.service';
 
-export class Pharmacy{
+export class Pharmacy {
   constructor(
-    public idPharmacy : string,
-    public namePharmacy : string,
-    public apiKeyPharmacy : string,
-    public endpoint : string
+    public idPharmacy: string,
+    public namePharmacy: string,
+    public apiKeyPharmacy: string,
+    public endpoint: string
   ) {
-    
+
   }
 
 }
 
-export class Feedback{
+export class Feedback {
   constructor(
-    public idFeedback : string,
-    public idPharmacy : string,
-    public contentFeedback : string,
+    public idFeedback: string,
+    public idHospital: string,
+    public contentFeedback: string,
   ) {
-    
   }
 
 }
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
 
 @Component({
   selector: 'app-create-feedback',
@@ -35,43 +40,57 @@ export class CreateFeedbackComponent implements OnInit {
   pharmacies: Pharmacy[] = [];
   selectedPharmacyId: string = '';
   contentFeedback: string = '';
+  http: any;
+  feedback: Feedback | undefined
 
-  constructor(private httpClient: HttpClient, private feedbackService: CreateFeedbackService) { 
-    
+  constructor(private httpClient: HttpClient, private feedbackService: CreateFeedbackService) {
+
   }
 
   ngOnInit(): void {
-      
+
 
     this.feedbackService.getPharmacies().subscribe((pharmacies: Pharmacy[]) => {
       this.pharmacies = pharmacies;
-      console.log(this.pharmacies);    
-      
-  });
+      console.log(this.pharmacies);
+
+    });
   }
 
-  
+
 
   //event handler for the select element's change event
-  selectChangeHandlerId (event: any) {
+  selectChangeHandlerId(event: any) {
     this.selectedPharmacyId = event.target.value;
   }
 
-  selectChangeHandlerContent (event: any) {
+  selectChangeHandlerContent(event: any) {
     this.contentFeedback = event.target.value;
-  }
-
-  end() : void {
-
-    var feedback = {
-      idFeedback: "test",
-      idPharmacy: this.selectedPharmacyId,
-      contextFeedback: this.contentFeedback};
-
-      console.log(feedback);
+    console.log(this.contentFeedback);
 
   }
 
-  
+  send(): void {
+
+    this.feedback = {
+      idFeedback: "testWorks",
+      idHospital: this.selectedPharmacyId,
+      contentFeedback: this.contentFeedback
+    };
+
+    console.log(this.feedback);
+    this.addFeedback(this.feedback)
+
+  }
+
+  addFeedback(feedback: Feedback) {
+
+    console.log(feedback)
+    return this.httpClient.post('http://localhost:64677/api/feedback', feedback, httpOptions); // DOES NOT WORK
+
+  }
+
 
 }
+
+
--- Frontend/src/app/integration/feedback/feedback.service.ts
+++ Frontend/src/app/integration/feedback/feedback.service.ts
@@ -4,14 +4,20 @@ import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
-  })
-  export class FeedbackService {
-      private baseUrl: string = 'http://localhost:44298/api/pharmacies';
-  
-      constructor(private http: HttpClient) { }
-  
-      getPharmacies(): Observable<any> {
-        
-          return this.http.get(this.baseUrl);
-      }
-  }
+})
+export class FeedbackService {
+    private baseUrl: string = 'http://localhost:44298/api/pharmacies';
+    private baseUrlFeedback: string = 'http://localhost:44298/api/feedback/find/';
+
+    constructor(private http: HttpClient) { }
+
+    getPharmacies(): Observable<any> {
+
+        return this.http.get(this.baseUrl);
+    }
+
+    getResponsesByPharmacy(idPharmacy: string): Observable<any> {
+
+        return this.http.get(this.baseUrlFeedback + idPharmacy);
+    }
+}
