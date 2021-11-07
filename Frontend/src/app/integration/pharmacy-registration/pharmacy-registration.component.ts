0dea6452afa30d3e25856f137fed6f7d6b1a9117
MarijaJankovicc <73437269+MarijaJankovicc@users.noreply.github.com>
2021-11-08 00:49:13 +0100

    feat: Pharmacy registration
    * Initial commit
    
    * feat: Add homepage for integration
    
    * feat: Add pharmacy registration
    
    * fix: Fix menu
    
    * Initial commit
    
    * feat: Add homepage for integration
    
    * feat: Add pharmacy registration
    
    * fix: Fix menu
    
    * feat: Data binding in pharmacy registration
    
    * Initial commit
    
    * feat: Add pharmacy registration
    
    * fix: Fix menu
    
    * Initial commit
    
    * feat: Add pharmacy registration
    
    * fix: Fix menu
    
    * feat: Data binding in pharmacy registration
    
    * fix: Fix double menu

--- Frontend/src/app/integration/pharmacy-registration/pharmacy-registration.component.ts
+++ Frontend/src/app/integration/pharmacy-registration/pharmacy-registration.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class PharmacyRegistration{
+  constructor(
+    public namePharmacy : string,
+    public apiKeyPharmacy : string,
+    public endpoint : string
+  ) {
+
+  }
+}
 
 @Component({
   selector: 'app-pharmacy-registration',
@@ -7,9 +18,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PharmacyRegistrationComponent implements OnInit {
 
-  constructor() { }
+  namePharmacy: string = '';
+  apiKeyPharmacy: string = '';
+  endpoint: string = '';
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  selectChangeHandlerName (event: any){
+    this.namePharmacy = event.target.value;
+    console.log(this.namePharmacy);
+  }
+
+  selectChangeHandlerApiKey (event: any){
+    this.apiKeyPharmacy = event.target.value;
+    console.log(this.apiKeyPharmacy);
+  }
+
+  selectChangeHandlerEndpoint (event: any){
+    this.endpoint = event.target.value;
+    console.log(this.endpoint);
+  }
+
+  send() : void {
+
+    var registration = {
+      namePharmacy: this.namePharmacy,
+      apiKeyPharmacy: this.apiKeyPharmacy,
+      endpoint: this.endpoint};
+      
+      console.log(registration);
+
   }
 
 }
