104febe65605b10f32e1d9aa6c9b9d553f5cea04
DejanBjelic <dejan.bjelic@uns.ac.rs>
2021-11-07 23:23:39 +0100

    feat: Added option to see response for sent feedback

--- Frontend/src/app/integration/feedback/feedback.component.ts
+++ Frontend/src/app/integration/feedback/feedback.component.ts
@@ -2,15 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FeedbackService } from './feedback.service';
 
-export class Pharmacy{
+export class Pharmacy {
   constructor(
-    public idPharmacy : string,
-    public namePharmacy : string,
-    public apiKeyPharmacy : string,
-    public endpoint : string
+    public idPharmacy: string,
+    public namePharmacy: string,
+    public apiKeyPharmacy: string,
+    public endpoint: string
   ) {
-    
+
   }
+}
+
+export class Feedback {
+  constructor(
+    public idFeedback: string,
+    public idPharmacy: string,
+    public contentFeedback: string,
+  ) {
+  }
+
 }
 
 @Component({
@@ -21,18 +31,35 @@ export class Pharmacy{
 export class FeedbackComponent implements OnInit {
 
   pharmacies: Pharmacy[] = [];
-  constructor(private httpClient: HttpClient, private feedbackService: FeedbackService) { 
-    
+  feedback: Feedback[] = [];
+  selectedPharmacyId: string = '';
+
+  constructor(private httpClient: HttpClient, private feedbackService: FeedbackService) {
+
   }
 
   ngOnInit(): void {
-    
+
     this.feedbackService.getPharmacies().subscribe((pharmacies: Pharmacy[]) => {
       this.pharmacies = pharmacies;
-      console.log(this.pharmacies);    
-      
-  });
-    
+      console.log(this.pharmacies);
+
+    });
+
+  }
+
+  selectChangeHandlerId(event: any) {
+    this.selectedPharmacyId = event.target.value;
+  }
+
+  viewResponse(): void {
+
+    this.feedbackService.getResponsesByPharmacy(this.selectedPharmacyId).subscribe((feedback: Feedback[]) => {
+      this.feedback = feedback;
+      console.log(this.feedback);
+
+    })
+
   }
 
 }
