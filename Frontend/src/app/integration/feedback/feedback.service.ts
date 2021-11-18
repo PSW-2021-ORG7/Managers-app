03514167c29888e051f0b017c4c7893a7a405ff8
Dejan Bjelic <iodum99@yahoo.com>
2021-11-18 21:32:37 +0100

    refactor: Replaced authentication service with local storage

--- Frontend/src/app/integration/feedback/feedback.service.ts
+++ Frontend/src/app/integration/feedback/feedback.service.ts
@@ -1,35 +1,24 @@
-import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, JsonpInterceptor } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-
-@Injectable()
-export class TokenInterceptor implements HttpInterceptor {
-  
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const ApiKey = 'ABC';
-      const modifiedReq = req.clone({ 
-        headers: req.headers.set('ApiKey', ApiKey),
-      });
-    return next.handle(modifiedReq);
-  }
-}
 
 @Injectable({
     providedIn: 'root'
 })
 export class FeedbackService {
-    private baseUrl: string = 'http://localhost:44298/api/pharmacies';
+    private baseUrl: string = 'http://localhost:44298/api/pharmacy';
     private baseUrlFeedback: string = 'http://localhost:44298/api/feedback/find/';
 
     constructor(private http: HttpClient) { }
 
     getPharmacies(): Observable<any> {
-
+        localStorage.setItem('ApiKey', JSON.stringify("ABC"));    
         return this.http.get(this.baseUrl);
     }
 
     getResponsesByPharmacy(idPharmacy: string): Observable<any> {
 
+        
         return this.http.get(this.baseUrlFeedback + idPharmacy);
     }
 }
--- Frontend/src/app/integration/http-interceptor.ts
+++ Frontend/src/app/integration/http-interceptor.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthenticationService } from './authentication-service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  private apiKey: string = 'ABC';
-  authService: AuthenticationService | undefined
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const ApiKey = "ABC";
+      const ApiKey = JSON.parse(localStorage.getItem('ApiKey') || '')
       const modifiedReq = req.clone({ 
-        headers: req.headers.set('ApiKey', this.apiKey),
+        headers: req.headers.set('ApiKey', ApiKey),
       });
     return next.handle(modifiedReq)
     .pipe();
