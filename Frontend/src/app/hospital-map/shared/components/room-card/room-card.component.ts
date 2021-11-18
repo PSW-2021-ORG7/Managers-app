0c8a40602d4791b0e11b3799d1f519fe9b78d43d
vujsa7 <vujisic27@gmail.com>
2021-11-19 00:10:59 +0100

    refactor: Fixed code smells

--- Frontend/src/app/hospital-map/shared/components/room-card/room-card.component.ts
+++ Frontend/src/app/hospital-map/shared/components/room-card/room-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Room, RoomStatus } from 'src/app/hospital-map/models/rooms/room.model';
 
 @Component({
