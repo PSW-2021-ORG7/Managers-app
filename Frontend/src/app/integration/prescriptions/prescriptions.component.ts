ded1b60cd17c9053399b812541f35b3877b064b4
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2021-12-09 21:48:59 +0100

    feat: Added page for prescriptions

---
+++ Frontend/src/app/integration/prescriptions/prescriptions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prescriptions',
+  templateUrl: './prescriptions.component.html',
+  styleUrls: ['./prescriptions.component.scss']
+})
+export class PrescriptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
