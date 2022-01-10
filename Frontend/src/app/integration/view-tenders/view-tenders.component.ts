5b0605f8fe755c3ddb1a5d13db8582d3e8e67db9
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2022-01-10 02:52:33 +0100

    feat: Data binding tender offers

--- Frontend/src/app/integration/view-tenders/view-tenders.component.ts
+++ Frontend/src/app/integration/view-tenders/view-tenders.component.ts
@@ -1,4 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class TenderOffer {
+  constructor(
+    public tenderOfferItems: TenderOfferItems[],
+    public priceForAllAvailable: string,
+    public priceForAllRequired: string,    
+    public totalNumberMissingMedicine: string,
+  ){}
+}
+
+export class TenderOfferItems {
+  constructor(
+    public medicineName: string,
+    public medicineDosage: number,
+    public availableQuantity: number,
+    public missingQuantity: number,
+    public priceForSingleEntity: number
+  ){}
+}
+
+export class Tender {
+  constructor(
+    public idTender: number,
+    public tenderKey: string,
+    public isActive: boolean,
+    public startDate: Date,
+    public endDate: Date,
+    public idWinnerPharmacy: number
+  ){}
+}
 
 @Component({
   selector: 'app-view-tenders',
@@ -7,15 +38,68 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ViewTendersComponent implements OnInit {
-  
-  public isCollapsed = false;
 
-  constructor() { }
+  selectedTenderId: string = ""
+  tenders: Tender[] = []
+
+  medicineName: string = ""
+  dosageInMilligrams: number = 0
+  availableQuantity: number = 0
+  missingQuantity: number = 0
+  priceForSingleEntity: number = 0 
+
+  tenderOfferItems: TenderOfferItems[] = []
+  priceForAllAvailable: string = "" 
+  priceForAllRequired: string = "" 
+  totalNumberMissingMedicine: string = ""
+
+  tenderOffers: TenderOffer[] = []
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
-  
 
+  selectChangeHandlerId(event: any) {
+    this.selectedTenderId = event.target.value;
+    console.log(this.selectedTenderId)
+
+  }
+
+  viewOffers(): void{
+    var tenderOfferItems = {
+      medicineName: this.medicineName,
+      dosageInMilligrams: this.dosageInMilligrams,
+      availableQuantity: this.availableQuantity,
+      missingQuantity: this.missingQuantity,
+      priceForSingleEntity: this.priceForSingleEntity,
+    };
+
+    var tenderOffer = {
+      tenderOfferItems: this.tenderOfferItems,
+      priceForAllAvailable : this.priceForAllAvailable,
+      priceForAllRequired: this.priceForAllRequired,
+    };
+
+  }
+  
+  acceptOffer(): void {
+
+    var tenderOfferItems = {
+      medicineName: this.medicineName,
+      dosageInMilligrams: this.dosageInMilligrams,
+      availableQuantity: this.availableQuantity,
+      missingQuantity: this.missingQuantity,
+      priceForSingleEntity: this.priceForSingleEntity,
+    };
+
+    var tenderOffer = {
+      tenderOfferItems: this.tenderOfferItems,
+      priceForAllAvailable : this.priceForAllAvailable,
+      priceForAllRequired: this.priceForAllRequired,
+    };
+
+  }
 
 
 }
