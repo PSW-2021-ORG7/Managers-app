05c837521389cdd37ffb11761ad001cbf4ffeede
Melanija Racic <racicmelanija@gmail.com>
2022-01-10 15:09:33 +0100

    feat: Add service class

---
+++ Frontend/src/app/hospital-map/shared/services/holiday.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Holiday } from '@app/hospital-map/models/shift/holiday.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HolidayService {
+  private baseUrl: string = environment.baseUrlHospital + 'holidays';
+
+  constructor(private http: HttpClient) { }
+
+  getHolidays(doctorId: string): Observable<Holiday[]> {
+    let params = new HttpParams().set('doctorId', doctorId)
+    return this.http.get<Holiday[]>(this.baseUrl, { params: params });
+  }
+
+  postHoliday(holiday: Holiday): Observable<any> {
+    return this.http.post<any>(this.baseUrl, holiday);
+  }
+
+  updateHoliday(holiday: Holiday): Observable<any> {
+    return this.http.put<any>(this.baseUrl + '/' + holiday.id, holiday);
+  }
+}
